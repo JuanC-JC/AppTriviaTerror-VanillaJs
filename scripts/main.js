532b1bc02ff7ba4991be92c668b340ac47e16ff1
Juan camilo <juancchaparro97@gmail.com>
2021-01-16 18:11:57 -0500

    sistema completamente funcional

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,47 @@
 const mainPage = document.getElementById("main-page")
 const mainCategorys = document.getElementById("main-categorys")
 const mainDinamic = document.getElementById("main-dinamic")
-const userName = document.getElementById("name-user")
+const userNameInput = document.getElementById("name-user")
 const buttonStart = document.getElementById("button-start")
+const formulario = document.getElementById("formulario-nombre")
 
 var puntaje = 0
 var indexQuestion;
 var defectTime = 5
 var chronometer;
 var selectedAnswer = undefined;
+var userName;
 
-buttonStart.addEventListener("click",startTrivia)
+buttonStart.addEventListener("click",(event)=>{ event.preventDefault(); startTrivia()})
+formulario.addEventListener("submit",(event)=>{ event.preventDefault(); startTrivia()})
 
+
+const lastScore = localStorage.getItem("lastScoreTrivia")
 
 function startTrivia(){
-    //esconder main-page hacer aparecer main-category
-    mainPage.classList.add("hidden")
-    mainCategorys.classList.remove("hidden")
-    insertCategorys()
+    userName =  userNameInput.value
+    if(userName == ""){
+        alert("ingrese un nombre")
+        userNameInput.focus();
+    }else{
+        //esconder main-page hacer aparecer main-category
+        mainPage.classList.add("hidden")
+        mainCategorys.classList.remove("hidden")
+        
+        insertCategorys()
+    }
+
 }
 
 
 function insertCategorys(){
 
     const categorys = [{name:"peliculas",
-                description:"lorem papus maximus uwu no se que escribir aca pero yolo",
-                imgUrl:".",
+                description:"¿las peliculas de terror son lo tuyo? <br> averiguemolo",
+                imgUrl:"",
                 preguntas:[
                             {
-                                pregunta:"quien soy yo?",
+                                pregunta:"en que pelicula, el protagonista se alimenta de las personas por placer?",
                                 respuestas:["un chamuco","un señorito","un pirilon"],
                                 respuesta:0
                             },
@@ -52,8 +65,8 @@ function insertCategorys(){
                         
                 },
 
-                {name:"geografia",
-                description:"lorem papus maximus de nuevo no se que escribir aca uwu masdasda",
+                {name:"juegos",
+                description:"¿que tan masoquista eres, nada como un buen scream?",
                 imgUrl:".",
                 preguntas:[
                             {
@@ -86,10 +99,52 @@ function insertCategorys(){
                             }
                         ]
                         
+                },
+
+                {name:"otro",
+                description:"lorem papus maximus de nuevo no se que escribir aca uwu masdasda",
+                imgUrl:".",
+                preguntas:[
+                            {
+                                pregunta:"quien soy yo?",
+                                respuestas:["un chamuco","un señorito","un pirilon"],
+                                respuesta:0
+                            },
+                            {
+                                pregunta:"Si buenas hay pan?",
+                                respuestas:["no sr wtf","un señorito","un pirilon"],
+                                respuesta:0
+                            }
+                        ]
+                        
+                },
+
+                {name:"otro",
+                description:"lorem papus maximus de nuevo no se que escribir aca uwu masdasda",
+                imgUrl:".",
+                preguntas:[
+                            {
+                                pregunta:"quien soy yo?",
+                                respuestas:["un chamuco","un señorito","un pirilon"],
+                                respuesta:0
+                            },
+                            {
+                                pregunta:"Si buenas hay pan?",
+                                respuestas:["no sr wtf","un señorito","un pirilon"],
+                                respuesta:0
+                            }
+                        ]
+                        
                 }
                 
             ]
 
+    const containerCategorysCarousel = createElement("div","",{class:"categorys-carousel"})
+    const containerCategorys = createElement("div","",{class:"categorys-container"})
+
+    containerCategorysCarousel.appendChild(containerCategorys)
+
+    let diferenType = 1
     //insertando html de categorys 
 
     //generar la categoria en html
@@ -97,10 +152,10 @@ function insertCategorys(){
         (category)=>{
 
             //creo un elemento html para el componente de categoria
-            const componentCategory = document.createElement("div")
-            componentCategory.classList.add("category")
+            const componentCategory = createElement("div","",{class:`category category--img-${diferenType}`})
+            containerCategorys.appendChild(componentCategory)
 
-            const htmlString = `<h1 class="category__title">${category.name}</h1>
+            const htmlString = `<h1 class="category__name">${category.name}</h1>
                                 <div class="category__img"><img src="${category.imgUrl}" alt=""></div>
                                 <div class="category__description"><p>${category.description}</p></div>`
 
@@ -108,19 +163,22 @@ function insertCategorys(){
             componentCategory.innerHTML += htmlString
 
             //creo un botton para no tener que hacer un selector luego de creado por innerhtml
-            const componentButtonCategory = createElement("button","pepe",{type:"button",class:"category__button button"})
+            const componentButtonCategory = createElement("div","",{class:"category__button"})
+
+      
+
+            componentCategory.addEventListener("click",()=>{insertCategory({...category})})
 
             //debo pasar un objeto copiado del original o lo modificara
-            componentButtonCategory.addEventListener("click",()=>{insertCategory({...category})})
+            // componentButtonCategory.addEventListener("click",()=>{insertCategory({...category})})
 
             componentCategory.appendChild(componentButtonCategory)
 
             //agrego el componente por el metodo agregarHijo ("permite no re-escribir esa parte del dom y perder referencias(eventos)")
-            mainCategorys.appendChild(componentCategory)
-    })
+            mainCategorys.appendChild(containerCategorysCarousel)
 
-    
-    //inyectar las categorias
+            diferenType == 2 ? diferenType=1: diferenType++;
+    })
 
     
 }
@@ -139,10 +197,10 @@ function insertCategory(category){
 
 
     //creo el componente categoria 
-    const categoryComponent = createElement("div","",{id:"category",class:"category"})
+    const categoryComponent = createElement("div","",{id:"category",class:"category-item"})
 
          //componentes de la categoria
-        const categoryCounter = createElement("div",`1/${category.preguntas.length}`,{id:"categoryCounter",class:"category_contador"})
+        const categoryCounter = createElement("div",`1/${category.preguntas.length}`,{id:"categoryCounter",class:"category__contador"})
         const categoryTimer = createElement("div","",{id:"categoryTimer",class:"category__timer"})
         const categoryTitle = createElement("h2",category.name,{class:"category__title"})
         const categoryQuestionContainer = createElement("div","",{id:"question-container",class:"category__question"})
@@ -157,7 +215,7 @@ function insertCategory(category){
     mainDinamic.appendChild(categoryComponent)
 
     //componente boton
-    const nextQuestionButton = createElement("div","next!",{id:"categoryNextQuestion",class:"button hidden"})
+    const nextQuestionButton = createElement("div","next",{id:"categoryNextQuestion",class:"category__next-question button hidden"})
     categoryComponent.appendChild(nextQuestionButton)
        
     nextQuestionButton.addEventListener("click",()=> {
@@ -215,15 +273,13 @@ function nextQuestion(listQuestions,availableTime=defectTime){
         indexQuestion ++
 
     }else{
-        clearInterval(chronometer)
+        localStorage.setItem("lastScoreTrivia",puntaje)
+        insertResultado(listQuestions)
 
-        mainCategorys.classList.remove("hidden")
-        categoryComponent.remove()
     }
 
 
 }
-
 
 //crea el componente de la pregunta
 function createQuestion(listQuestions,index){
@@ -248,7 +304,7 @@ function createQuestion(listQuestions,index){
     //agregar respuestas
     question.respuestas.forEach(respuesta=>{
 
-        const questionAnswer = createElement("button",respuesta,{class:"question__answer button",name:"answer"})
+        const questionAnswer = createElement("button",respuesta,{class:"question__answer button"})
 
         //funcion real, controlador
         questionAnswer.addEventListener("click",()=>validateAnswer(question,questionAnswer))
@@ -320,7 +376,52 @@ function setTimer(time,listQuestions){
 
 }
 
-function insertResultado(){
+function insertResultado(listQuestions){
+    let mensaje;
+    if(puntaje/(listQuestions.length*100) < 1){
+        mensaje = "Puedes hacerlo mejor!"
+    }else{
+        mensaje = "Puntaje perfecto, Felicitaciones!!, ve a por otra categoria"
+    }
     
-    const htmlString = ``
+    const htmlString =  `<div class="resultado">
+                            <h2 class="resultado__title">${userName} has obtenido un puntaje de ${puntaje}/${listQuestions.length*100} </h2>
+                            <h4 class="resultado__mensaje">${mensaje}</h4>
+                            <div class="button" id="play-againg">Play Againg!</div>
+                            <div class="button" id="new-player">New Player</div>
+                        </div>`
+
+
+
+    mainDinamic.innerHTML = htmlString
+
+
+    const playAgain = document.getElementById("play-againg")
+    const newPlayer = document.getElementById("new-player")
+
+
+    //nuevo menu
+    playAgain.addEventListener("click",()=>{
+        
+        mainCategorys.classList.remove("hidden")
+        mainDinamic.innerHTML = ""
+        mainDinamic.classList.add("hidden")
+        
+    })
+
+
+
+    //nuevo jugador
+    newPlayer.addEventListener("click",()=>{
+        userNameInput.value = ""
+        mainPage.classList.remove("hidden")
+        mainDinamic.classList.add("hidden")
+        mainDinamic.innerHTML = ""
+        mainCategorys.innerHTML = ""
+        userNameInput.focus();
+    })
+
+
+
 }
+
