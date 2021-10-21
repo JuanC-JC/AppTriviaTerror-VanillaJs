e7742a87de402086045f567b6952e04c17dfbc84
JuanCJC <juancchaparro97@gmail.com>
2021-10-21 10:44:37 -0500

    move categories json to a other file

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+
 const mainPage = document.getElementById("main-page");
 const mainCategorys = document.getElementById("main-categorys");
 const mainDinamic = document.getElementById("main-dinamic");
@@ -13,7 +14,7 @@ var selectedAnswer = undefined;
 var userName;
 
 //preload images for categories
-function preloadImage(){
+( () => {
 
 		const preload = (url) =>{
 			const res = document.createElement("link");
@@ -31,9 +32,8 @@ function preloadImage(){
 
 		listImages.forEach(img => preload(img))
 
-}
+})();
 
-preloadImage()
 
 const viewLoading = () =>{
 
@@ -77,226 +77,226 @@ function startTrivia() {
 function insertCategorys() {
 	htmlstringRepresentation = "";
 
-	const categorys = [
-		{
-			name: "peliculas",
-			description: "¿las peliculas de terror son lo tuyo? averiguemolo",
-			preguntas: [
-				{
-					pregunta: "¿Cuantas peliculas de la franquicia saw se emitieron?",
-					respuestas: [
-						"5 peliculas",
-						"3 peliculas",
-						"7 peliculas",
-						"9 peliculas",
-					],
-					respuesta: 3,
-				},
-				{
-					pregunta: "¿En que pelicula el protagonista viola a su propio hijo?",
-					respuestas: [
-						"hanibal lecter",
-						"dracula 2",
-						"serbian film",
-						"el hoyo",
-					],
-					respuesta: 2,
-				},
-				{
-					pregunta:
-						"¿La Famosa frase i see dead people(veo gente muerta), de que pelicula es?",
-					respuestas: [
-						"memorias de un eterno resplandor",
-						"dracula",
-						"sexto sentido",
-						"alien covenant",
-					],
-					respuesta: 2,
-				},
-				{
-					pregunta: "¿Cómo se llama la niña del Aro?",
-					respuestas: [
-						"rachel", 
-						"samara", 
-						"samantha",
-						"riven"
-					],
-					respuesta: 1,
-				}
-			],
-		},
+	// const categorys = [
+	// 	{
+	// 		name: "peliculas",
+	// 		description: "¿las peliculas de terror son lo tuyo? averiguemolo",
+	// 		preguntas: [
+	// 			{
+	// 				pregunta: "¿Cuantas peliculas de la franquicia saw se emitieron?",
+	// 				respuestas: [
+	// 					"5 peliculas",
+	// 					"3 peliculas",
+	// 					"7 peliculas",
+	// 					"9 peliculas",
+	// 				],
+	// 				respuesta: 3,
+	// 			},
+	// 			{
+	// 				pregunta: "¿En que pelicula el protagonista viola a su propio hijo?",
+	// 				respuestas: [
+	// 					"hanibal lecter",
+	// 					"dracula 2",
+	// 					"serbian film",
+	// 					"el hoyo",
+	// 				],
+	// 				respuesta: 2,
+	// 			},
+	// 			{
+	// 				pregunta:
+	// 					"¿La Famosa frase i see dead people(veo gente muerta), de que pelicula es?",
+	// 				respuestas: [
+	// 					"memorias de un eterno resplandor",
+	// 					"dracula",
+	// 					"sexto sentido",
+	// 					"alien covenant",
+	// 				],
+	// 				respuesta: 2,
+	// 			},
+	// 			{
+	// 				pregunta: "¿Cómo se llama la niña del Aro?",
+	// 				respuestas: [
+	// 					"rachel", 
+	// 					"samara", 
+	// 					"samantha",
+	// 					"riven"
+	// 				],
+	// 				respuesta: 1,
+	// 			}
+	// 		],
+	// 	},
 
-		{
-			name: "juegos",
-			description: "¿que tan masoquista eres, nada como un buen scream?",
-			preguntas: [
-				{
-					pregunta: "¿cómo se llama la terrorifica institucion mental que sirve de escenario a outlast?",
-					respuestas: [
-						"hospital de filadelfia",
-						"hospital whittingham",
-						"hospital severalls",
-						"mount masive",
-					],
-					respuesta: 3,
-				},
-				{
-					pregunta: "¿cúal es el nombre de la estacion espacial que debemos explorar en Alien:Isolation?",
-					respuestas: [
-						"nostromo",
-						"sevastopol",
-						"weyland",
-						"yutani",
-					],
-					respuesta: 1,
-				},
-				{
-					pregunta: "¿cómo se llama el equipo de fuerzas especiales protagonista del primer Resident Evil?",
-					respuestas: [
-						"alpha",
-						"victory",
-						"pícaro",
-						"apollo",
-					],
-					respuesta: 0,
-				},
-				{
-					pregunta: "¿qúe significan las siglas de F.E.A.R?",
-					respuestas: [
-						"First Encounter Action Response",
-						"First Encounter Action Recon",
-						"First Encounter Assault Recon",
-						"Finest Endure Action Response",
-					],
-					respuesta: 2,
-				},
-			],
-		},
+	// 	{
+	// 		name: "juegos",
+	// 		description: "¿que tan masoquista eres, nada como un buen scream?",
+	// 		preguntas: [
+	// 			{
+	// 				pregunta: "¿cómo se llama la terrorifica institucion mental que sirve de escenario a outlast?",
+	// 				respuestas: [
+	// 					"hospital de filadelfia",
+	// 					"hospital whittingham",
+	// 					"hospital severalls",
+	// 					"mount masive",
+	// 				],
+	// 				respuesta: 3,
+	// 			},
+	// 			{
+	// 				pregunta: "¿cúal es el nombre de la estacion espacial que debemos explorar en Alien:Isolation?",
+	// 				respuestas: [
+	// 					"nostromo",
+	// 					"sevastopol",
+	// 					"weyland",
+	// 					"yutani",
+	// 				],
+	// 				respuesta: 1,
+	// 			},
+	// 			{
+	// 				pregunta: "¿cómo se llama el equipo de fuerzas especiales protagonista del primer Resident Evil?",
+	// 				respuestas: [
+	// 					"alpha",
+	// 					"victory",
+	// 					"pícaro",
+	// 					"apollo",
+	// 				],
+	// 				respuesta: 0,
+	// 			},
+	// 			{
+	// 				pregunta: "¿qúe significan las siglas de F.E.A.R?",
+	// 				respuestas: [
+	// 					"First Encounter Action Response",
+	// 					"First Encounter Action Recon",
+	// 					"First Encounter Assault Recon",
+	// 					"Finest Endure Action Response",
+	// 				],
+	// 				respuesta: 2,
+	// 			},
+	// 		],
+	// 	},
 
-		{
-			name: "test",
-			description:
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
-			preguntas: [
-				{
-					pregunta: "lorem ipsum dolor sit amet",
-					respuestas: [
-						"lorem ipsum",
-						"dolor sit amet",
-						"consectur adpiscing elit",
-						"sed do eiusmod",
-					],
-					respuesta: 3,
-				},
-				{
-					pregunta: "lorem ipsum dolor sit amet",
-					respuestas: [
-						"lorem ipsum",
-						"dolor sit amet",
-						"consectur adpiscing elit",
-						"sed do eiusmod",
-					],
-					respuesta: 0,
-				},
-				{
-					pregunta: "lorem ipsum dolor sit amet",
-					respuestas: [
-						"lorem ipsum",
-						"dolor sit amet",
-						"consectur adpiscing elit",
-						"sed do eiusmod",
-					],
-					respuesta: 2,
-				},
-				{
-					pregunta: "lorem ipsum dolor sit amet",
-					respuestas: [
-						"lorem ipsum",
-						"dolor sit amet",
-						"consectur adpiscing elit",
-						"sed do eiusmod",
-					],
-					respuesta: 2,
-				},
-				{
-					pregunta: "lorem ipsum dolor sit amet",
-					respuestas: [
-						"lorem ipsum",
-						"dolor sit amet",
-						"consectur adpiscing elit",
-						"sed do eiusmod",
-					],
-					respuesta: 2,
-				},
-			],
-		},
+	// 	{
+	// 		name: "test",
+	// 		description:
+	// 			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+	// 		preguntas: [
+	// 			{
+	// 				pregunta: "lorem ipsum dolor sit amet",
+	// 				respuestas: [
+	// 					"lorem ipsum",
+	// 					"dolor sit amet",
+	// 					"consectur adpiscing elit",
+	// 					"sed do eiusmod",
+	// 				],
+	// 				respuesta: 3,
+	// 			},
+	// 			{
+	// 				pregunta: "lorem ipsum dolor sit amet",
+	// 				respuestas: [
+	// 					"lorem ipsum",
+	// 					"dolor sit amet",
+	// 					"consectur adpiscing elit",
+	// 					"sed do eiusmod",
+	// 				],
+	// 				respuesta: 0,
+	// 			},
+	// 			{
+	// 				pregunta: "lorem ipsum dolor sit amet",
+	// 				respuestas: [
+	// 					"lorem ipsum",
+	// 					"dolor sit amet",
+	// 					"consectur adpiscing elit",
+	// 					"sed do eiusmod",
+	// 				],
+	// 				respuesta: 2,
+	// 			},
+	// 			{
+	// 				pregunta: "lorem ipsum dolor sit amet",
+	// 				respuestas: [
+	// 					"lorem ipsum",
+	// 					"dolor sit amet",
+	// 					"consectur adpiscing elit",
+	// 					"sed do eiusmod",
+	// 				],
+	// 				respuesta: 2,
+	// 			},
+	// 			{
+	// 				pregunta: "lorem ipsum dolor sit amet",
+	// 				respuestas: [
+	// 					"lorem ipsum",
+	// 					"dolor sit amet",
+	// 					"consectur adpiscing elit",
+	// 					"sed do eiusmod",
+	// 				],
+	// 				respuesta: 2,
+	// 			},
+	// 		],
+	// 	},
 
-		{
-			name: "alguno",
-			description:
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
-			preguntas: [
-				{
-					pregunta: "lorem ipsum dolor sit amet",
-					respuestas: [
-						"lorem ipsum",
-						"dolor sit amet",
-						"consectur adpiscing elit",
-						"sed do eiusmod",
-					],
-					respuesta: 1,
-				},
-				{
-					pregunta: "lorem ipsum dolor sit amet",
-					respuestas: [
-						"lorem ipsum",
-						"dolor sit amet",
-						"consectur adpiscing elit",
-						"sed do eiusmod",
-					],
-					respuesta: 0,
-				},
-				{
-					pregunta: "lorem ipsum dolor sit amet",
-					respuestas: [
-						"lorem ipsum",
-						"dolor sit amet",
-						"consectur adpiscing elit",
-						"sed do eiusmod",
-					],
-					respuesta: 2,
-				},
-			],
-		},
+	// 	{
+	// 		name: "alguno",
+	// 		description:
+	// 			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+	// 		preguntas: [
+	// 			{
+	// 				pregunta: "lorem ipsum dolor sit amet",
+	// 				respuestas: [
+	// 					"lorem ipsum",
+	// 					"dolor sit amet",
+	// 					"consectur adpiscing elit",
+	// 					"sed do eiusmod",
+	// 				],
+	// 				respuesta: 1,
+	// 			},
+	// 			{
+	// 				pregunta: "lorem ipsum dolor sit amet",
+	// 				respuestas: [
+	// 					"lorem ipsum",
+	// 					"dolor sit amet",
+	// 					"consectur adpiscing elit",
+	// 					"sed do eiusmod",
+	// 				],
+	// 				respuesta: 0,
+	// 			},
+	// 			{
+	// 				pregunta: "lorem ipsum dolor sit amet",
+	// 				respuestas: [
+	// 					"lorem ipsum",
+	// 					"dolor sit amet",
+	// 					"consectur adpiscing elit",
+	// 					"sed do eiusmod",
+	// 				],
+	// 				respuesta: 2,
+	// 			},
+	// 		],
+	// 	},
 
-		{
-			name: "sin ideas",
-			description:
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
-			preguntas: [
-				{
-					pregunta: "lorem ipsum dolor sit amet",
-					respuestas: [
-						"lorem ipsum",
-						"dolor sit amet",
-						"consectur adpiscing elit",
-						"sed do eiusmod",
-					],
-					respuesta: 3,
-				},
-				{
-					pregunta: "lorem ipsum dolor sit amet",
-					respuestas: [
-						"lorem ipsum",
-						"dolor sit amet",
-						"consectur adpiscing elit",
-						"sed do eiusmod",
-					],
-					respuesta: 0,
-				},
-			],
-		},
-	];
+	// 	{
+	// 		name: "sin ideas",
+	// 		description:
+	// 			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+	// 		preguntas: [
+	// 			{
+	// 				pregunta: "lorem ipsum dolor sit amet",
+	// 				respuestas: [
+	// 					"lorem ipsum",
+	// 					"dolor sit amet",
+	// 					"consectur adpiscing elit",
+	// 					"sed do eiusmod",
+	// 				],
+	// 				respuesta: 3,
+	// 			},
+	// 			{
+	// 				pregunta: "lorem ipsum dolor sit amet",
+	// 				respuestas: [
+	// 					"lorem ipsum",
+	// 					"dolor sit amet",
+	// 					"consectur adpiscing elit",
+	// 					"sed do eiusmod",
+	// 				],
+	// 				respuesta: 0,
+	// 			},
+	// 		],
+	// 	},
+	// ];
 
 	const componentCategorys = createElement("div", "", { class: "categorys" });
 	const CategorysCarousel = createElement(
